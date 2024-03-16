36ff2c05bfaa619e8e20ed04a194ba5604df63b3
abdoul karim <abdoulkarimsoumana62@gmail.com>
2024-03-16 12:26:34 +0100

    i cree a view

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Container from './components/Container';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>bonsoir</Text>
-      <StatusBar style="auto" />
+      <Container/>
     </View>
   );
 }
